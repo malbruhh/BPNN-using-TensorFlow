1668403967d7ec4e2a8947069b7f143514ed317d
malbruhh <ikhmaldanish741@gmail.com>
2026-01-05 15:59:56 +0800

    finished

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# create 2-3 graph
-#1 Loss curve(learning curve)-MSE,binary cross entropy
-#2 accuracy over time(epoch)
-#3 confusion matrix heatmap
-
-
-#preprocess data
-#one-hot encoding (for nominal)
-#Feature scaling
-#class imbalance
-#data split
-
 #Preprocessing Steps:
 #1. Standardization: Rename columns and convert to lowercase.
 
